1c2afd1b905f2578634212128d27e105345a0733
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2024-01-24 23:51:46 +0530

    Update triggers.sh

--- triggers.sh
+++ triggers.sh
@@ -1,1 +1,35 @@
 #!/bin/bash
+
+set -x
+
+aws_account_id=$(aws #store the aws account ID in a variable)
+
+echo "AWS Account id :"
+
+#AWS region and bucket name
+
+aws_region = 
+bucket_name=
+lambda_func_name=
+role_name
+email_address
+
+# Create IAM role 
+
+role_response = $(aws iam create-role --role-name ){
+    #Details 
+}
+
+# Extract the role ARN from JSON response and then store it in a variabl
+role_arn=$(echo "$role_response" | jq -r ',Role.Arn')
+
+# Print the role ARN
+echo "Role ARN: $role_arn"
+
+# Create the S3 bucket 
+bucket_output=$(aws s3api create-bucket --bucket "$bucket_name"  --region "$aws_region")
+
+# Print the output from the variable
+echo "Bucket creation output : $bucket_output"
+
+
